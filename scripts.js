976dc11a318075a110e2af4f6e430b11f911bedd
Yuri Guilherme Santana de Sá <yuri.luana2016@gmail.com>
2022-08-14 23:22:38 -0300

    sidebar-responsive(v0)

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const aside = document.querySelector("aside")
+const p = document.querySelectorAll("p")
+const logo = document.querySelector(".logo")
+
+const ocultarParagrafos = (opacity) => {
+    p.forEach((p => {
+        p.style.opacity = opacity;
+    }))
+}
+
+
+aside.onclick = () => {
+    aside.classList.toggle("active")
+    if (aside.classList.contains("active") == true) {
+        ocultarParagrafos(100)
+    } else {
+        ocultarParagrafos(0)
+    }
+}
